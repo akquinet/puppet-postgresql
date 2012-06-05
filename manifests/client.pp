017a29325f9217dd8ff327ffa073fcd39eb6d2b8
SHaselbauer <sarah.haselbauer@akquinet.de>
2012-06-05 21:24:25 +0200

    updated class to ensure client installation according to server pkgname changes

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,5 +1,11 @@
-class postgresql::client($version) {
-  package { "postgresql-client-${version}":
-    ensure => present,
-  }
+class postgresql::client ($version) {
+	$clpkgname = $::operatingsystem ? {
+		'redhat' => "postgresql",
+		'centos' => "postgresql",
+		default => "postgresql-${version}",
+	}
+	package {
+		"$clpkgname" :
+			ensure => present,
+	}
 }
