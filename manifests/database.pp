7fd9df8a404cba9dd808ca75f489718771ddd73f
saheba <mail-at-saheba-dot-net>
2013-04-27 18:03:49 +0200

    added template option to database.pp (creation section)

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,10 +1,21 @@
-define postgresql::database($owner, $charset='UTF8', $ensure=present) {
+define postgresql::database(
+  $owner, 
+  $charset='UTF8', 
+  $ensure=present, 
+  $template=undef
+) {
   $dbexists = "psql -ltA | grep '^${name}|'"
 
   if $ensure == 'present' {
-
+    
+    if $template == undef {
+      $templateDB = ''
+    } else {
+      $templateDB = " -T $template"
+    }
+    
     exec { "createdb $name":
-      command => "createdb -O ${owner} -E ${charset} ${name}",
+      command => "createdb -O ${owner} -E ${charset}$templateDB ${name}",
       user    => 'postgres',
       unless  => $dbexists,
       path => ["/bin", "/sbin", "/usr/bin"],
